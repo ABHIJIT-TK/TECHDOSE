4c4107e405d0966beb70185b6e5af975f620efaf
ABHIJIT TK <101189245+ABHIJIT-TK@users.noreply.github.com>
2024-01-03 10:50:00 +0530

    Sort color.py

---
+++ Array/Sort color.py
@@ -0,0 +1,10 @@
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # for i in range(len(nums)):
+        #     for j in range(0,len(nums)-i-1):
+        #         if nums[j]>nums[j+1]:
+        #             nums[j],nums[j+1]=nums[j+1],nums[j]
+        return nums.sort()      
